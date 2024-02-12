6777a458ad9c35349a687c95954f92ceda206137
unknown <429192-2>
2024-02-12 09:22:28 +0300

    lab2

---
+++ lab2/lab2/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using lab1.Classes;
+using lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace lab2
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Test(string name, string surname)
+        {
+            ViewBag.Title = "MyCafe";
+            ViewBag.CafeName = "MyCafe";
+            ViewBag.Greeting = "Привет, " + name + " " + surname;
+
+            ViewBag.Spisok = new List<string>() {"Один", "Два", "Три" };
+            return View();
+        }
+        public ViewResult Index()
+        {
+            ViewBag.Title = "MyCafe";
+            ViewBag.CafeName = "MyCafe";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult DrinkForm()
+        {
+            ViewBag.Title = "Drinks";
+            return View();
+        }
+        [HttpPost]
+        public ViewResult DrinkForm(DrinkData drinkData)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (drinkData.DrinkTypeEn)
+                {
+                    case DrinkType.Tea:
+                        drinkData.beverage = new Tea(); break;
+                    case DrinkType.Juice:
+                        drinkData.beverage = new Juice(); break;
+                    case DrinkType.Coffee:
+                        drinkData.beverage = new Coffee(); break;
+                    case DrinkType.Alcohol:
+                        drinkData.beverage = new Alcohol(); break;
+                }
+                drinkData.beverage = new Sugar(drinkData.beverage, drinkData.Sugar);
+                drinkData.beverage = new Milk(drinkData.beverage, drinkData.Milk);
+                ViewBag.Title = "Bill";
+                ViewBag.Cost = drinkData.beverage.cost();
+                return View("Bill", drinkData);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ lab2/lab2/Models/DrinkData.cs
@@ -0,0 +1,26 @@
+﻿using lab1.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab2.Models
+{
+    public enum DrinkType
+    {
+        Tea,
+        Coffee,
+        Juice,
+        Alcohol
+    }
+    public class DrinkData
+    {
+        public DrinkType DrinkTypeEn { get; set; }
+        public Beverage beverage;
+        [Required(ErrorMessage = "Не забудьте ввести количество молока!")]
+        public int Milk { get; set; }
+        [Required(ErrorMessage = "Не забудьте ввести количество сахара!")]
+        public int Sugar { get; set; }
+    }
+}
